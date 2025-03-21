0a2233cbaba9220b10e06ba572d7e48262e5e30e
Mohamad <ghost4ft@gmail.com>
2025-03-21 20:59:53 +0330

    Initial swagger

--- src/middlewares/swagger.js
+++ src/middlewares/swagger.js
@@ -14,7 +14,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: `${process.env.APP_HOST}:${process.env.APP_PORT}`,
             },
         ],
         components: {
